6723d274d8cd238502c60c0fca1584d1df7038cd
nikitha <gullapalli.nikitha@gmail.com>
2020-11-30 21:13:20 -0800

    working: recycler with with design ghuildline grid and keylines

--- app/src/main/java/com/example/android/dynamicsurfacesdemo/CollapsingToolbarActivity.java
+++ app/src/main/java/com/example/android/dynamicsurfacesdemo/CollapsingToolbarActivity.java
@@ -1,17 +1,26 @@
 package com.example.android.dynamicsurfacesdemo;
 
-import android.app.Activity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
-public class CollapsingToolbarActivity extends Activity {
+public class CollapsingToolbarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.collapsingtoolbar_activity);
 
-        ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle(getString(R.string.CollapsingToolbarexampleTitle));
+        CollapsingToolbarLayout ab = findViewById(R.id.collapsing_toolbar_layout);
+        Toolbar app_bar = findViewById(R.id.app_bar);
+        setSupportActionBar(app_bar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ab.setTitle(getString(R.string.CollapsingToolbarexampleTitle));
 
     }
+
+
 }
